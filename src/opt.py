64f33cd62e4ee515834dc2d723101f8d4571075a
Alexsey Soluyanov <al.soluyanov@rambler.ru>
2020-03-28 17:56:27 +0300

    added the opt class

---
+++ src/opt.py
@@ -0,0 +1,18 @@
+class opt(object):
+    float lb = None
+    float rb = None
+    float r = None
+    int max_iter = None
+    
+    def __init__(self):
+        pass
+
+    def Minimize(self):
+        return
+
+    def Piyavsky(self):
+        return
+
+    def Strongin(self):
+        return
+ 
